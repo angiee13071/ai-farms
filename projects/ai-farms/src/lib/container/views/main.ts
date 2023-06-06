cbd028364fb9ad2bd19b304ff1d8a25b2194d575
angiee.carrion <angiee.carrion@agrodatai.com>
2023-06-06 10:58:48 -0500

    iniciando rpoyecto

--- projects/ai-farms/src/lib/container/views/main.ts
+++ projects/ai-farms/src/lib/container/views/main.ts
@@ -1,6 +1,6 @@
 // Obtén los elementos necesarios del DOM
-const container = document.getElementById("container");
-const elementList = document.getElementById("element-list");
+const container = document.querySelector(".card-container") as HTMLElement;
+const elementList = document.querySelector(".list") as HTMLElement;
 
 // Variable para realizar el seguimiento del último índice de elemento cargado
 let lastIndex = 0;
@@ -13,14 +13,22 @@ function loadMoreElements() {
 
         // Genera algunos elementos de ejemplo
         for (let i = lastIndex; i < lastIndex + 10; i++) {
-            const listItem = document.createElement("li");
-            listItem.textContent = `Elemento ${i + 1}`;
-            newElements.push(listItem);
+            const card = document.createElement("div");
+            card.classList.add("card");
+            card.innerHTML = `
+        <img class="photo" src="ruta_de_la_imagen_${i}.jpg" alt="Imagen">
+        <p class="name">Nombre del elemento ${i + 1}</p>
+        <div>
+          <button class="button_add"></button>
+          <p class="text_button">Agregar</p>
+        </div>
+      `;
+            newElements.push(card);
         }
 
         // Agrega los nuevos elementos al DOM
         newElements.forEach((element) => {
-            elementList.appendChild(element);
+            elementList?.appendChild(element);
         });
 
         // Actualiza el último índice de elemento cargado
@@ -30,18 +38,18 @@ function loadMoreElements() {
 
 // Función para detectar cuándo se ha llegado al final del scroll
 function handleScroll() {
-    const scrollTop = container.scrollTop;
-    const containerHeight = container.offsetHeight;
-    const scrollHeight = container.scrollHeight;
+    const scrollTop = container?.scrollTop;
+    const containerHeight = container?.offsetHeight;
+    const scrollHeight = container?.scrollHeight;
 
     // Verifica si el scroll está cerca del final (ajusta el valor "50" según tus necesidades)
-    if (scrollHeight - scrollTop - containerHeight < 50) {
+    if (scrollHeight && scrollTop && containerHeight && scrollHeight - scrollTop - containerHeight < 50) {
         loadMoreElements();
     }
 }
 
 // Agrega el evento de desplazamiento al contenedor
-container.addEventListener("scroll", handleScroll);
+container?.addEventListener("scroll", handleScroll);
 
 // Carga los primeros elementos
 loadMoreElements();
